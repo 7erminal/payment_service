61a2ac1611191df850fb42b9d5a5aa7700d03d2e
Bede Abbe <bede.abbe@gmail.com>
2025-05-28 13:41:37 +0000

    changes

--- controllers/payments.go
+++ controllers/payments.go
@@ -62,6 +62,7 @@ func (c *PaymentsController) Post() {
 				logs.Error("Error getting user ", err.Error())
 			}
 			if status, err := models.GetStatusByName("PENDING"); err == nil {
+				logs.Info("Payment reference number is " + v.ReferenceNumber)
 				var payment models.Payments = models.Payments{Transaction: transaction, PaymentProof: v.PaymentProofUrl, ReferenceNumber: v.ReferenceNumber, InitiatedBy: v.InitiatedBy, Sender: &sender, Reciever: &receiver, Amount: float64(v.Amount), PaymentMethod: paymentMethod, Status: status, PaymentAccount: 0, DateCreated: time.Now(), DateModified: time.Now(), CreatedBy: v.InitiatedBy, ModifiedBy: v.InitiatedBy, Active: 1}
 				if _, err := models.AddPayments(&payment); err == nil {
 					// Send to Account service to debit and credit
