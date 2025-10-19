2871b0c3c475865ab1d6f99ff14a02b0bc121279
Bede Abbe <bede.abbe@gmail.com>
2025-10-19 07:13:32 +0000

    changes

--- controllers/payment_methods.go
+++ controllers/payment_methods.go
@@ -81,7 +81,7 @@ func (c *Payment_methodsController) GetAll() {
 	var sortby []string
 	var order []string
 	var query = make(map[string]string)
-	var limit int64 = 10
+	var limit int64 = 100
 	var offset int64
 
 	// fields: col1,col2,entity.col3
